7a5071c9482b28d6791610bbc261a240077caa1a
Luz Virginia Aramburo Alvarez <luzaramburo13@gmail.com>
2023-12-05 17:52:15 -0700

    hide keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 export const app = initializeApp({
-  apiKey: 'AIzaSyB6uHnhocwg15ubmiBGnrr0Vz3NIb28how',
-  authDomain: 'word-soup-71429.firebaseapp.com',
-  databaseURL: 'https://word-soup-71429-default-rtdb.firebaseio.com',
-  projectId: 'word-soup-71429',
-  storageBucket: 'word-soup-71429.appspot.com',
-  messagingSenderId: '126585871128',
-  appId: '1:126585871128:web:9f9112f344899381cbdc6d',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 });
 
 export const auth = getAuth(app);
