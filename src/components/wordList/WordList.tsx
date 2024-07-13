9ed35e9f08d3246b2f8351f4e6d418189087234f
Luz Virginia Aramburo Alvarez <luzaramburo13@gmail.com>
2024-07-12 18:46:30 -0700

    word list css details

--- src/components/wordList/WordList.tsx
+++ src/components/wordList/WordList.tsx
@@ -4,10 +4,10 @@ import { useAppSelector } from '@/store/hooks.ts';
 export const WordList = () => {
   const wordsList = useAppSelector(state => state.game.wordList);
   return (
-    <ul className="md:col-span-1 border border-gray-400 p-4 grid grid-cols-2 md:grid-cols-1 md:order-first">
+    <ul className="md:col-span-1 border border-gray-400 p-4 grid grid-cols-2 md:flex flex-col md:gap-3 md:order-first">
       { wordsList.length > 0 && wordsList.map((item, index) => (
         <li
-          className={classNames('text-xl mb-3 uppercase decoration-[3px]', { 'line-through': item.found })}
+          className={classNames('text-xl uppercase decoration-[3px]', { 'line-through': item.found })}
           key={index}
         >{item.word}</li>
       )) }
